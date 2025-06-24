09530be596891639f16a6494852bcbbbc07c7baa
Filipe <filipesobrinho18@gmail.com>
2025-06-24 20:49:22 -0300

    feat/criacao-das-apis-game-e-instituicao

--- backend/src/services/InstituicaoService.js
+++ backend/src/services/InstituicaoService.js
@@ -1,41 +1,41 @@
-import GameRepository from '../repositories/GameRepository.js';
+import InstituicaoRepository from '../repositories/InstituicaoRepository.js';
 
 const validTypes = ["P" ,"G"];
 const validStatuses = ["O", "C"];
-const getAllGames = async () => {
-      const Game = await GameRepository.getAllGames();
-      return Game ;
+const getAllInstituicao = async () => {
+      const Instituicao = await InstituicaoRepository.getAllInstituicao();
+      return Instituicao ;
 };
 
-const getGameById = async ( {id} ) => {
+const getInstituicaoById = async ( {id} ) => {
         id = null;
         await validateIdParam({ id: id });
-        const Game = await GameRepository.getGameById({ id: id});
-        return Game ;
+        const Instituicao = await InstituicaoRepository.getInstituicaoById({ id: id});
+        return Instituicao ;
 };
 
-const saveGame = async ({ time_a_id, time_b_id, horario_inicio, endereco }) => {
+const saveInstituicao = async ({ time_a_id, time_b_id, horario_inicio, endereco }) => { //Mudar com a tabela instituicao
         await validateIdParam({ time_a_id: time_a_id });
         await validateIdParam({ time_b_id: time_b_id });
         await validateTypeParam({ horario_inicio: horario_inicio });
         await validateStatusParam({ endereco: endereco });
-        GameRepository.saveGame({ time_a_id: time_a_id, time_b_id: time_b_id, horario_inicio: horario_inicio, endereco: endereco });
+        InstituicaoRepository.saveInstituicao({ time_a_id: time_a_id, time_b_id: time_b_id, horario_inicio: horario_inicio, endereco: endereco });
         
 };
-//RELACIONAR CAMPO DE ARBITRO NA TABELA JOGO
-const updateGameById = async ({ time_a_id, time_b_id, horario_inicio, endereco }) => {
+
+const updateInstituicaoById = async ({ time_a_id, time_b_id, horario_inicio, endereco }) => {
       await validateIdParam({ time_a_id: time_a_id });
       await validateIdParam({ time_b_id: time_b_id });
       await validateTypeParam({ horario_inicio: horario_inicio });
       await validateStatusParam({ endereco: endereco });
-      GameRepository.updateGameById({ time_a_id: time_a_id, time_b_id: time_b_id, horario_inicio: horario_inicio, endereco: endereco });
+      InstituicaoRepository.updateInstituicaoById({ time_a_id: time_a_id, time_b_id: time_b_id, horario_inicio: horario_inicio, endereco: endereco });
 };
-//RELACIONAR CAMPO DE ARBITRO NA TABELA JOGO
 
 
-const deleteGameById = async ({ id }) => {
+
+const deleteInstituicaoById = async ({ id }) => {
       await validateIdParam({ id: id });
-      GameRepository.deleteGameById({ id: id});
+      InstituicaoRepository.deleteInstituicaoById({ id: id});
       
 };
 
@@ -43,7 +43,7 @@ const validateIdParam = async ({ id }) => {
       if (!id) {
             throw { code: 400, message: "ID parameter is required." };
       }
-      //Validar se o Id existe na tabela instituicao
+   
 }
 
 
@@ -66,4 +66,4 @@ const validateStatusParam = async ({ status }) => {
       }
 };
 
-export default { getAllGames, getGameById, saveGame, updateGameById, deleteGameById };
+export default { getAllInstituicao, getInstituicaoById, saveInstituicao, updateInstituicaoById, deleteInstituicaoById };
